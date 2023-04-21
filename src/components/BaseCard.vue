a8687d7b88c66b20989da7dc70333cc8de70d551
Davitpurceladze <purceladzedavit@gmail.com>
2023-04-21 11:08:34 +0400

    create BaseCard slot and use it on CashGame page

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card">
+    <hr class="line">
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+.line {
+  position: absolute;
+  display: block;
+  height: 4px;
+  margin-bottom: 4rem;
+  margin-top: 4px;
+  background-color: #EF5A21;
+  width: 30%;
+  border: 4px;
+  border-bottom-left-radius: 4px;
+  border-bottom-right-radius: 4px;
+}
+.card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem 1rem;
+  height: 4rem;
+  border-radius: 16pt;
+  background-color: #2C3234;
+}
+
+@media (min-width: 401px) {
+  .card {
+    margin: 2rem 3rem
+  }
+}
+
+</style>
--- src/views/CashGameContent.vue
+++ src/views/CashGameContent.vue
@@ -1,11 +1,21 @@
 <template>
-  <h1>Cash Games</h1> 
+  <div>
+    <BaseCard> <div class="isTopBox">1ლ რეიქი = 1 ქულას </div></BaseCard>
+    <div class="isTableBox">main</div>
+    <div class="isBottomBox">bottom</div>
+  </div>
 </template>
 
 <script setup>
+import BaseCard from '../components/BaseCard.vue';
 
 </script>
 
 <style scoped>
-
+.isTopBox {
+  color: white;
+  font-size: 16pt;
+  font-family: November GeLC;
+  font-weight: bold;
+}
 </style>
