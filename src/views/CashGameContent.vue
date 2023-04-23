34e0f6e68c8cbb4561ad254bffc17ae0c682cd44
Davitpurceladze <purceladzedavit@gmail.com>
2023-04-23 15:49:14 +0400

    create tournament info card

--- src/views/CashGameContent.vue
+++ src/views/CashGameContent.vue
@@ -1,16 +1,50 @@
 <template>
   <div>
     <LeaderBoard></LeaderBoard>
-    <div class="isBottomBox" style="color: white;">bottom</div>
+    <BaseCard class="isTournamentInfo">
+      <div class="isTournamentParagraph">
+        <p class="isMainInfo">დამატებით შედგება, ქეშგეიმის, ტურნირების და სპინ პოკერის 12 Side ლიდერბორდი</p>
+        <p class="isSideInfo">* Side ლიდერბორდების შესახებ დეტალური ინფორმაცია იხილეთ პოკერის ლობიში.</p>
+      </div>
+    </BaseCard>
   </div>
 </template>
 
 <script setup>
 import LeaderBoard from '../pageComponents/cashgameComponents/LeaderBoard.vue'
+import BaseCard from '../components/BaseCard.vue';
 </script>
 
 <style scoped>
+.isTournamentInfo {
+  margin: 2rem 1rem 2rem 1rem;
+}
  
+.isTournamentParagraph {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+}
+
+.isMainInfo {
+  font-size: 16pt;
+  font-family: November GeLC;
+  font-weight: bold;
+  color: #FFFFFF;
+}
+
+.isSideInfo {
+  font-size: 12pt;
+  color: #FFFFFF;
+  font-family: HelveticaNeue ;
+}
+
  
+@media (min-width: 401px) {
+  .isTournamentInfo {
+    margin: 2rem 3rem 2rem 3rem;
+  }
+}
  
 </style>
