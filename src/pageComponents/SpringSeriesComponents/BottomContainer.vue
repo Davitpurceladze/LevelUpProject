97a0b16a8dccb666d1bc149d16e35a42a6b6a59c
Davitpurceladze <purceladzedavit@gmail.com>
2023-04-25 19:28:54 +0400

    add tournament info in springSeries

--- src/pageComponents/SpringSeriesComponents/BottomContainer.vue
+++ src/pageComponents/SpringSeriesComponents/BottomContainer.vue
@@ -1,11 +1,48 @@
 <template>
-  <h1>bottom container</h1>
+    <div class="container">
+      <BaseCard class="isTournamentInfo">
+      <div class="isTournamentParagraph">
+        <p class="isMainInfo">დამატებით შედგება, ქეშგეიმის, ტურნირების და სპინ პოკერის 12 Side ლიდერბორდი</p>
+        <p class="isSideInfo">* Side ლიდერბორდების შესახებ დეტალური ინფორმაცია იხილეთ პოკერის ლობიში.</p>
+      </div>
+    </BaseCard>
+    </div>
 </template>
 
 <script setup>
+import BaseCard from '../../components/BaseCard.vue'
 
 </script>
 
 <style scoped>
+.isTournamentInfo {
+  margin: 2rem 1rem 2rem 1rem;
+}
+ 
+.isTournamentParagraph {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+}
 
+.isMainInfo {
+  font-size: 16pt;
+  font-family: November GeLC;
+  font-weight: bold;
+  color: #FFFFFF;
+}
+
+.isSideInfo {
+  font-size: 12pt;
+  color: #FFFFFF;
+  font-family: HelveticaNeue ;
+}
+
+ 
+@media (min-width: 577px) {
+  .isTournamentInfo {
+    margin: 2rem 3rem 2rem 3rem;
+  }
+}
 </style>
